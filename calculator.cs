5c041243f5862a17e893a4757cda4c4328c6316e
Jawahar <jawaharib@gmail.com>
2019-09-25 06:32:03 +0530

    Inital Commit for Calc

---
+++ calculator.cs
@@ -0,0 +1,40 @@
+using System;
+
+// Creating a simple Calculator class
+/// <summary>
+/// Summary description for Class1.
+/// </summary>
+public class CalCulator
+{
+	// Square function
+	public int Square(int num)
+		{
+			retrun num * num;
+		}
+		
+	// Add two integers and // TO DOs the sum
+	public int Add(int a, int b )
+		{
+			// TO DO 
+		}
+		
+	// Add two integers and // TO DOs the sum
+	public double Add(double a, double b )
+		{
+			// TO DO
+		}
+		
+	// Multiply two integers and retuns the result
+	public int Multiply(int a, int b )
+		{
+			// TO DO 
+		}
+		
+	// Subtracts small number from big number
+	public int Subtract(int a, int b )
+		{
+			
+			// TO DO
+		}
+}
+
